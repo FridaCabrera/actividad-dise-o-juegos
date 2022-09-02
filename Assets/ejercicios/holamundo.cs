33702a4c39728ad49cae5303dba1640d5466d075
ROCIO CABRERA OSORIO <rocaosmac@MacBook-Pro-de-ROCIO.local>
2022-09-02 13:35:39 -0500

    First commit, empty project and .gitignore

---
+++ Assets/ejercicios/holamundo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class holamundo : MonoBehaviour
+{
+
+    void Start() {
+        Debug.Log("Hola mundo!");
+    }
+
+    void Update()
+    {
+
+    }
+}
